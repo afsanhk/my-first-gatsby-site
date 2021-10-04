472d8dc60d978e4aea648e0f4efbfb4f2f0387ef
Afsanul Haque Khan <afsanhk@gmail.com>
2021-10-04 17:50:23 -0400

    Add first plugin and load image using StaticImage component from the Gatsby image plugin as per part 3 of the tutorial.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
-// Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="A random image of an elephant silhouette" src="../images/firstImage.jpeg" />
     </Layout>
   );
 };
 
-// Step 3: Export your component
 export default IndexPage;
