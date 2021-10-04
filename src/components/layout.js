b7e4599c165c6043d63527699a90917a2507ac1f
Afsanul Haque Khan <afsanhk@gmail.com>
2021-10-04 18:27:36 -0400

    Use GraphQL and useStaticQuery hook built into Gatsby to access site name from gatsby-config.js as per part 4 of the tutorial.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,25 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 // Kebab case is automaticall converted to camelCase in JS using .module.css
-import { container, heading, navLinks, navLinkItem, navLinkText } from "./layout.module.css";
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
